0a9ea3aa84768a33e6da495500d8102138bea90c
Paulo Gabriel <138730369+paulogab2601@users.noreply.github.com>
2024-11-11 10:30:38 -0300

    aulas
    falta completar a questao

---
+++ A18-Prova.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct provas
+{
+    char nome[50];
+    char resposta[31];
+    int mat;
+} provas;
+
+int main()
+{
+    provas *provAlunos;
+    int n_alunos;
+    printf("Digite o numero de alunos: ");
+    scanf("%d", &n_alunos);
+
+
+    return 0;
+}
+
+provas cadastroprova(provas* prova){
+
+
+}
