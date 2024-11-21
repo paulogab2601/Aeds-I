f2aa65492c866b6bba9883cc9c3344a0074e05f8
Paulo Gabriel <138730369+paulogab2601@users.noreply.github.com>
2024-11-21 09:47:50 -0300

    +arquivos

---
+++ A21-Arquivo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int strleng(FILE *, int *);
+
+int main()
+{
+    int num_chars = 0, *count_A = 0;
+    FILE *arquivo = fopen("texto.txt", "r");
+
+    num_chars = strleng(arquivo, &count_A);
+
+    fclose(arquivo);
+
+    printf("Chars: %d, A: %d", num_chars, count_A);
+
+    return 0;
+}
+int strleng(FILE *texto, int *count_A)
+{
+
+    int count = 0;
+    char c;
+
+    while ((c = fgetc(texto)) != EOF)
+    {
+        count++;
+        if (c == 'a' || c == 'A')
+        {
+            (*count_A)++;
+        }
+    }
+
+    return count;
+}
